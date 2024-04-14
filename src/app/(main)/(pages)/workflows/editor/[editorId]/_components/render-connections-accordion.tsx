f94a86e4f4980397b19ebafff4eb75212e7cf5f9
Rohit Nandi <rohitnandibmp@gmail.com>
2024-04-15 00:40:18 +0530

    after adding multiple-selector and modified connection accordion

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/render-connections-accordion.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/render-connections-accordion.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { Connection } from "@/lib/types";
+import { useNodeConnections } from "@/providers/connections-provider";
 import { EditorState } from "@/providers/editor-provider";
+import { useZeroStore } from "@/store";
 import React from "react";
 
 type Props = {
@@ -18,6 +20,16 @@ const RenderConnectionAccoridon = ({ connection, state }: Props) => {
         alwaysTrue,
         slackSpecial,
     } = connection;
+    const { nodeConnection } = useNodeConnections();
+    const { slackChannels, selectedSlackChannels, setSelectedSlackChannels } =
+        useZeroStore();
+    const connectionData = (nodeConnection as any)[connectionKey];
+
+    const isConnected =
+        alwaysTrue ||
+        (nodeConnection[connectionKey] &&
+            accessTokenKey &&
+            connectionData[accessTokenKey]);
 
     return <div>RenderConnectionAccoridon</div>;
 };
